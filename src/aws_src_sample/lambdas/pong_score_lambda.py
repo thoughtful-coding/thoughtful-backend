848ecda41f203f33beb714c835faea7f6ce9d30c
infinity13637 <infinity13637@gmail.com>
2024-04-22 10:04:16 -0500

    a

--- src/aws_src_sample/lambdas/pong_score_lambda.py
+++ src/aws_src_sample/lambdas/pong_score_lambda.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import logging
 import os
-
-
+import json
+from aws_src_sample.utils.apig_utils import get_event_body
 from aws_src_sample.s3.object_inputter import ObjectInputter
 from aws_src_sample.s3.object_outputter import ObjectOutputter
 from aws_src_sample.dynamodb.pong_score_table import PongScoreTable
@@ -12,6 +12,7 @@ _LOGGER.setLevel(logging.INFO)
 from aws_src_sample.utils.aws_env_vars import (
     get_pong_score_table_name,
     get_output_bucket_name,
+    get_region,
 )
 
 
@@ -29,11 +30,40 @@ class PongScoreHandler:
         self.object_outputter.put(bucket=output_bucket_name, key="data", contents=self.pong_score_table.get_top_five())
         return {}
 
+    def handle2(self, event: dict) -> dict:
+        try:
+            input_data = get_event_body(event)
+            self.pong_score_table.set_value(item_key="abc", item_value=9)
+            return {
+                "statusCode": 201,
+                "headers": {
+                    "Content-Type": "application/json",
+                },
+                "body": json.dumps(
+                    {
+                        "message": "File processed successfully.",
+                    }
+                ),
+            }
+        except Exception as e:
+            print(f"Error processing file: {str(e)}")
+
+            return {"statusCode": 500, "body": json.dumps("Failed to process the file.")}
+
 
 # placehold
 
 
-def pong_score_lambda_handler(event: dict, context) -> dict:
+def pong_score_get_lambda_handler(event: dict, context) -> dict:
+    lh = PongScoreHandler(
+        # whatever
+        ObjectOutputter(),
+        PongScoreTable(get_pong_score_table_name()),
+    )
+    return lh.handle(event)
+
+
+def pong_score_post_lambda_handler(event: dict, context) -> dict:
     lh = PongScoreHandler(
         # whatever
         ObjectOutputter(),
